532c7aec4b556529b4d7444f054ef30c49cbd13f
worsleyj <josephworsley23@gmail.com>
2023-12-12 09:41:44 -0500

    implemented take_turn method, user input

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,19 +1,34 @@
 class Game
   def initialize
-    @player1 = "Joe"
-    @player2 = "player2"
-    @board = Array.new(3) {Array.new(3, "[]")}
+    @player1 = 'Joe'
+    @player2 = 'player2'
+    @board = Array.new(3) {Array.new(3, '[]') }
   end
 
   def display_board(board)
     p board[0]
     p board[1]
     p board[2]
+    puts "\n"
+  end
+
+  def take_turn(player, board)
+    puts 'Enter x Coordinate (1-3)'
+    x_coord = gets.chomp.to_i - 1
+    puts 'Enter y Coordinate (1-3)'
+    y_coord = gets.chomp.to_i - 1
+
+    board[x_coord][y_coord] = player
+    display_board(@board)
   end
 
   def start_game
-    puts "Player One vs Player Two"
+    winner = ''
+    puts 'Player One vs Player Two'
+    puts 'The Game Begins!'
     display_board(@board)
+
+    take_turn("x", @board) # while winner == ''
   end
 end
 
