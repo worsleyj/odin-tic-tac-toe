ce0aa4109598013d9a1395dd6793faf3d554d5d7
worsleyj <josephworsley23@gmail.com>
2023-12-12 13:19:43 -0500

    win checking needs fixing, will come later after next project

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -17,22 +17,26 @@ class Game
   def take_turn(board)
     make_move(@board)
     check_winner()
-    next_player(@current)
+    # next_player(@current)
     display_board(@board)
   end
 
   def make_move(board)
-    puts 'Enter x Coordinate (1-3)'
-    x_coord = gets.chomp.to_i - 1
-    puts 'Enter y Coordinate (1-3)'
-    y_coord = gets.chomp.to_i - 1
+    begin
+      puts 'Enter x Coordinate (1-3)'
+      x_coord = gets.chomp.to_i - 1
+      puts 'Enter y Coordinate (1-3)'
+      y_coord = gets.chomp.to_i - 1
 
-    if board[x_coord][y_coord] != '[]'
-      puts 'Those Coordinates are invalid, try again.'
-      display_board(@board)
-      make_move(@board)
-    else
-      board[x_coord][y_coord] = @current end
+      if board[x_coord][y_coord] != '[]'
+        puts 'Those Coordinates are already taken, try again.'
+        display_board(@board)
+        make_move(@board)
+      else
+        board[x_coord][y_coord] = @current end
+    rescue
+        puts 'Those Coordinates are invalid, try again.'
+    end
   end
 
   def next_player(current)
@@ -41,6 +45,8 @@ class Game
                else
                  @player1
                end
+    puts "---------------------------------"
+    puts @current + " is making their move."
   end
 
   def check_winner()
@@ -50,31 +56,20 @@ class Game
   end
 
   def check_rows(board)
-    if board[0][0] == board[0][1] && board[0][1] == board[0][2]
-      @winner = board[0][0]
-    elsif board[1][0] == board[1][1] && board[1][1] == board[1][2]
-      @winner = board[1][0]
-    elsif board[2][0] == board[2][1] && board[2][1] == board[2][2]
-      @winner = board[2][0]
-    end
+    @winner = board[0][0] if board[0][0] == board[0][1] && board[0][1] == board[0][2]
+    @winner = board[1][0] if board[1][0] == board[1][1] && board[1][1] == board[1][2]
+    @winner = board[2][0] if board[2][0] == board[2][1] && board[2][1] == board[2][2]
   end
 
   def check_columns(board)
-    if board[0][0] == board[1][0] && board[1][0] == board[2][0]
-      @winner = board[0][0]
-    elsif board[0][1] == board[1][1] && board[1][1] == board[2][1]
-      @winner = board[0][1]
-    elsif board[0][2] == board[1][2] && board[1][2] == board[2][2]
-      @winner = board[0][2]
-    end
+    @winner = board[0][0] if board[0][0] == board[1][0] && board[1][0] == board[2][0]
+    @winner = board[0][1] if board[0][1] == board[1][1] && board[1][1] == board[2][1]
+    @winner = board[0][2] if board[0][2] == board[1][2] && board[1][2] == board[2][2]
   end
 
   def check_diagonals(board)
-    if board[0][0] == board[1][1] && board[1][1] == board[2][2]
-      @winner = board[0][0]
-    elsif board[2][0] == board[1][1] && board[1][1] == board[0][2]
-      @winner = board[2][0]
-    end
+    @winner = board[0][0] if board[0][0] == board[1][1] && board[1][1] == board[2][2]
+    @winner = board[2][0] if board[2][0] == board[1][1] && board[1][1] == board[0][2]
   end
 
   def start_game
