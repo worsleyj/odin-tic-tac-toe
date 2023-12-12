f79ea376268aa671b9a8db7156ca60a25465faa7
worsleyj <josephworsley23@gmail.com>
2023-12-12 10:01:41 -0500

    current player is changed between turns

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,7 +1,8 @@
 class Game
   def initialize
-    @player1 = 'Joe'
-    @player2 = 'player2'
+    @player1 = 'X'
+    @player2 = 'O'
+    @current = @player1
     @board = Array.new(3) {Array.new(3, '[]') }
   end
 
@@ -12,14 +13,15 @@ class Game
     puts "\n"
   end
 
-  def take_turn(player, board)
+  def take_turn(board)
     begin
       puts 'Enter x Coordinate (1-3)'
       x_coord = gets.chomp.to_i - 1
       puts 'Enter y Coordinate (1-3)'
       y_coord = gets.chomp.to_i - 1
 
-      board[x_coord][y_coord] = player
+      board[x_coord][y_coord] = @current
+      next_player(@current)
     rescue StandardError
       puts '** Invalid Coordinates entered, please try again. **'
     end
@@ -27,13 +29,19 @@ class Game
     display_board(@board)
   end
 
+  def next_player(current)
+    if @current == @player1
+      @current = @player2
+    else @current = @player1 end
+  end
+
   def start_game
     winner = ''
-    puts 'Player One vs Player Two'
     puts 'The Game Begins!'
     display_board(@board)
 
-    take_turn('x', @board) # while winner == ''
+    take_turn(@board) # while winner == ''
+    take_turn(@board) # while winner == ''
   end
 end
 
